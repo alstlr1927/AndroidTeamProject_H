866d6233e14d4c5ee5a8ac1a3538161b1a213d31
skawogus95 <skawogus95@naver.com>
2021-03-26 15:19:37 +0900

    남재현 : Test

--- app/src/main/java/com/cookandroid/androidteamproject_h/Fragment_activity.java
+++ app/src/main/java/com/cookandroid/androidteamproject_h/Fragment_activity.java
@@ -9,8 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.airbnb.lottie.LottieAnimationView;
-
 public class Fragment_activity extends Fragment {
 
     // 생성자 생성할것
