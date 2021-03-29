ee8fa7ae0d2ccf654d1c1c5b1f1a7c8412ba9c67
alstlr1927 <dnalstlr1927@icloud.com>
2021-03-29 15:49:14 +0900

    우현성 : 바텀 네비게이션 뷰에 프래그먼트 4개 연결

--- app/src/main/java/com/cookandroid/androidteamproject_h/BottomMenuActivity.java
+++ app/src/main/java/com/cookandroid/androidteamproject_h/BottomMenuActivity.java
@@ -1,14 +1,74 @@
 package com.cookandroid.androidteamproject_h;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomMenuActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_menu);
+
+        findViewByIdFunc();
+
+        setChangeFragment(0);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.frag1 :
+                        setChangeFragment(1);
+                        break;
+                    case R.id.frag2 :
+                        setChangeFragment(2);
+                        break;
+                    case R.id.frag3 :
+                        setChangeFragment(3);
+                        break;
+                    case R.id.frag4 :
+                        setChangeFragment(4);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setChangeFragment(int position) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+        switch(position) {
+            case 0 :
+                ft.replace(R.id.frameLayout, new Fragment1());
+                break;
+            case 1 :
+                ft.replace(R.id.frameLayout, new Fragment2());
+                break;
+            case 2 :
+                ft.replace(R.id.frameLayout, new Fragment3());
+                break;
+            case 3 :
+                ft.replace(R.id.frameLayout, new Fragment4());
+                break;
+        }
+        ft.commit();
+    }
+
+    private void findViewByIdFunc() {
+        navigationView = findViewById(R.id.navigationView);
+        frameLayout = findViewById(R.id.frameLayout);
     }
 }
