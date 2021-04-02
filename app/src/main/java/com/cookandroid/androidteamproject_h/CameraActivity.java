87a5165d6b774a9aaab6ac09c0c0cc1df0375d3a
alstlr1927 <dnalstlr1927@icloud.com>
2021-04-03 00:20:10 +0900

    우현성 : Manifest 재수정

--- app/src/main/java/com/cookandroid/androidteamproject_h/CameraActivity.java
+++ app/src/main/java/com/cookandroid/androidteamproject_h/CameraActivity.java
@@ -1,12 +1,22 @@
 package com.cookandroid.androidteamproject_h;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,7 +24,10 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     private ImageView imgPhoto;
     private EditText edtContent;
 
+    private Uri photoUri;
     private String title;
+    private String imageFile;
+    private String imageFilepath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +51,44 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnCamera :
 
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+
+                    File file = null;
+
+                    try {
+                        file = createImageFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(file != null) {
+                        photoUri = FileProvider.getUriForFile(CameraActivity.this, getPackageName(), file);
+                    }
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+
+                }
+                break;
+        }
+    }
+
+    private File createImageFile() throws IOException {
+
+        SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd_hhmmss");
+
+        String str = date.format(new Date());
+        imageFile = "pic_" + str + "_";
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFile, ".jpg", dir);
+        imageFilepath = image.getAbsolutePath();
+
+        return image;
     }
 
 }
