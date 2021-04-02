a6ac5ff362555019a5a0454a60c3fdda908235b8
alstlr1927 <dnalstlr1927@icloud.com>
2021-04-02 23:56:38 +0900

    우현성 : Manifest 수정

--- app/src/main/java/com/cookandroid/androidteamproject_h/CameraActivity.java
+++ app/src/main/java/com/cookandroid/androidteamproject_h/CameraActivity.java
@@ -3,12 +3,42 @@ package com.cookandroid.androidteamproject_h;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCamera;
+    private ImageView imgPhoto;
+    private EditText edtContent;
+
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        findViewByIdFunc();
     }
+
+    private void findViewByIdFunc() {
+        btnCamera = findViewById(R.id.btnCamera);
+        imgPhoto = findViewById(R.id.imgPhoto);
+        edtContent = findViewById(R.id.edtContent);
+
+        btnCamera.setOnClickListener(this);
+        imgPhoto.setOnClickListener(this);
+        edtContent.setOnClickListener(this);
+
+        title = getIntent().getStringExtra("title");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
